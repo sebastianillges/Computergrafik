d9a17352a6c8b3b5f4f3c3891cf0d2bc2591997e
sebastianillges <sebastian.illges@gmail.com>
2022-11-14 21:47:08 +0100

    Added Uebung2 files

---
+++ Uebung2/main.cpp
@@ -0,0 +1,216 @@
+// Sample code for Übung 2
+
+#include "vec.h"
+#include "mat.h"
+
+// might be you have to swith to
+// #include "glut.h" depending on your GLUT installation
+#include "Uebung2/Uebung2/glut.h"
+
+////////////////////////////////////////////////////////////
+//
+// system relevant global variables
+//
+
+// window width and height (choose an appropriate size)
+const int g_iWidth  = 400;
+const int g_iHeight = 400;
+
+// global variable to tune the timer interval
+int g_iTimerMSecs;
+
+//
+/////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+//
+// private, global variables ... replace by your own ones
+//
+// some global state variables used to describe ...
+float g_iPos;		// ... position and ...
+float g_iPosIncr;	// ... position increment (used in display1)
+
+CVec2i g_vecPos;		// same as above but in vector form ...
+CVec2i g_vecPosIncr;	// (used in display2)
+//
+/////////////////////////////////////////////////////////////
+
+// function to initialize our own variables
+void init () 
+{
+
+	// init timer interval
+	g_iTimerMSecs = 10;
+
+	// init variables for display1
+	g_iPos     = 0;
+	g_iPosIncr = 2;
+
+	// init variables for display2
+	int aiPos    [2] = {0, 0};
+	int aiPosIncr[2] = {2, 2};
+	g_vecPos.setData (aiPos);
+	g_vecPosIncr.setData (aiPosIncr);
+}
+
+// function to initialize the view to ortho-projection
+void initGL () 
+{
+	glViewport (0, 0, g_iWidth, g_iHeight);	// Establish viewing area to cover entire window.
+
+	glMatrixMode (GL_PROJECTION);			// Start modifying the projection matrix.
+	glLoadIdentity ();						// Reset project matrix.
+	glOrtho (-g_iWidth/2, g_iWidth/2, -g_iHeight/2, g_iHeight/2, 0, 1);	// Map abstract coords directly to window coords.
+
+	// tell GL that we draw to the back buffer and
+	// swap buffers when image is ready to avoid flickering
+	glDrawBuffer (GL_BACK);
+
+	// tell which color to use to clear image
+	glClearColor (0,0,0,1);
+}
+
+
+int min (int a, int b) { return a>b? a: b; }
+// timer callback function
+void timer (int value) 
+{
+	///////
+	// update your variables here ...
+	//
+
+	int size2 = min (g_iWidth, g_iHeight) / 2;
+
+	// variables for display1 ...
+	if (g_iPos<=-size2 || g_iPos>=size2) g_iPosIncr = -g_iPosIncr;
+	g_iPos += g_iPosIncr;
+
+	// variables for display2 ...
+	if (g_vecPos(1)<=-size2 || g_vecPos(1)>=size2) g_vecPosIncr = -g_vecPosIncr; 
+	g_vecPos += g_vecPosIncr;
+
+	//
+	///////
+
+	// the last two lines should always be
+	glutPostRedisplay ();
+	glutTimerFunc (g_iTimerMSecs, timer, 0);	// call timer for next iteration
+}
+
+// display callback function
+void display1 (void) 
+{
+
+	glClear (GL_COLOR_BUFFER_BIT);
+
+	///////
+	// display your data here ...
+	//
+
+	glBegin (GL_TRIANGLES);
+		glColor3f (1,0,0);
+		glVertex2i (g_iPos, 0);
+		glColor3f (0,1,0);
+		glVertex2i (-g_iPos, g_iPos);
+		glColor3f (0,0,1);
+		glVertex2i (-g_iPos, -g_iPos);
+	glEnd ();
+
+	//
+	///////
+
+	// In double buffer mode the last
+	// two lines should alsways be
+	glFlush ();
+	glutSwapBuffers (); // swap front and back buffer
+}
+
+// display callback function
+void display2 (void) 
+{
+	glClear (GL_COLOR_BUFFER_BIT);
+
+	///////
+	// display your data here ...
+	//
+
+	glBegin (GL_QUADS);
+		glColor3f (1,0,0);
+		glVertex2i (-g_vecPos(1), -g_vecPos(2));
+		glColor3f (0,1,0);
+		glVertex2i (g_vecPos(1), -g_vecPos(2));
+		glColor3f (0,0,1);
+		glVertex2i (g_vecPos(1), g_vecPos(2));
+		glColor3f (1,1,0);
+		glVertex2i (-g_vecPos(1), g_vecPos(2));
+	glEnd ();
+
+	//
+	///////
+
+	// In double buffer mode the last
+	// two lines should alsways be
+	glFlush ();
+	glutSwapBuffers (); // swap front and back buffer
+}
+
+void keyboard (unsigned char key, int x, int y) 
+{
+	switch (key) {
+		case 'q':
+		case 'Q':
+			exit (0); // quit program
+			break;
+		case '1':
+			glutDisplayFunc (display1);
+			//glutPostRedisplay ();	// not needed since timer triggers redisplay
+			break;
+		case '2':
+			glutDisplayFunc (display2);
+			//glutPostRedisplay ();	// not needed since timer triggers redisplay
+			break;
+		default:
+			// do nothing ...
+			break;
+	};
+}
+
+/*
+void bhamLine (Point p1, Point p2, Color c) 
+{
+	glBegin (GL_POINTS);
+	glColor3f (c.r, c.g, c.b);
+	// ...
+
+		// implement bhamLine here and use
+		// glVertex2i (x, y);
+		// to draw a pixel
+	
+	// ...
+	glEnd ();
+}
+*/
+
+int main (int argc, char **argv) 
+{
+	glutInit (&argc, argv);
+	// we have to use double buffer to avoid flickering
+	// TODO: lookup "double buffer", what is it for, how is it used ...
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+	glutCreateWindow ("Übung 2");
+
+	init ();	// init my variables first
+	initGL ();	// init the GL (i.e. view settings, ...)
+
+	// assign callbacks
+	glutTimerFunc (10, timer, 0);
+	glutKeyboardFunc (keyboard);
+	glutDisplayFunc (display1);
+	// you might want to add a resize function analog to
+	// Übung1 using code similar to the initGL function ...
+
+	// start main loop
+	glutMainLoop ();
+
+	return 0;
+}
