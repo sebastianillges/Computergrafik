90bae45ce5a4903ae28785c68bbd79abff860519
MSchmidtHTWG <92575109+MSchmidtHTWG@users.noreply.github.com>
2022-11-15 17:41:20 +0100

    first try

--- Uebung2/main.cpp
+++ Uebung2/main.cpp
@@ -1,5 +1,6 @@
 // Sample code for �bung 2
 
+#include <math.h>
 #include "vec.h"
 #include "mat.h"
 
@@ -7,6 +8,8 @@
 // #include "glut.h" depending on your GLUT installation
 #include "glut.h"
 
+void rotate_origin(double *, double *, double);
+void rotate(double, double, double*, double*, double);
 ////////////////////////////////////////////////////////////
 //
 // system relevant global variables
@@ -27,22 +30,22 @@ int g_iTimerMSecs;
 // private, global variables ... replace by your own ones
 //
 // some global state variables used to describe ...
-float sun_x;
-float sun_y;
-float earth_x;
-float earth_y;
-float moon_x;
-float moon_y;
-float angle_earth_moon;
-float angle_sun_earth;
-float angle_incr_earth_moon;
-float angle_incr_sun_earth;
+double sun_x;
+double sun_y;
+double earth_x;
+double earth_y;
+double moon_x;
+double moon_y;
+double angle_earth_moon;
+double angle_sun_earth;
+double angle_incr_earth_moon;
+double angle_incr_sun_earth;
 
-CVec3f sun;
-CVec3f earth;
-CVec3f moon;
-CMat3f rotate_earth_sun; // the homogeneous transformation matrix of earths rotation around the sun
-CMat3f rotate_moon_earth_sun; // the homogeneous transformation matrix of the moons rotation around the earth AND the sun
+CVec3d sun;
+CVec3d earth;
+CVec3d moon;
+CMat3d rotate_earth_sun; // the homogeneous transformation matrix of earths rotation around the sun
+CMat3d rotate_moon_earth_sun; // the homogeneous transformation matrix of the moons rotation around the earth AND the sun
 
 float g_iPos;		// ... position and ...
 float g_iPosIncr;	// ... position increment (used in display1)
@@ -51,6 +54,19 @@ CVec2i g_vecPos;		// same as above but in vector form ...
 CVec2i g_vecPosIncr;	// (used in display2)
 //
 /////////////////////////////////////////////////////////////
+
+void setpoint(int x, int y) {
+	glBegin(GL_QUADS);
+		glColor3f (1,0,0);
+		glVertex2i (x+10, y+10);
+		glColor3f (0,1,0);
+		glVertex2i (x-10, y+10);
+		glColor3f (0,0,1);
+		glVertex2i (x+10, y-10);
+		glColor3f (1,1,0);
+		glVertex2i (x-10, y-10);
+	glEnd();
+}
 
 // function to initialize our own variables
 void init () 
@@ -71,18 +87,18 @@ void init ()
 
 	sun_x = 0;
 	sun_y = 0;
-	earth_x = 0;
-	earth_y = g_iWidth / 2;
-	moon_x = 0;
-	moon_y = g_iWidth / (4.0/3.0);
+	earth_x = g_iWidth / 4;
+	earth_y = 100;
+	moon_x = g_iWidth / (8.0/3.0);
+	moon_y = 0;
 	angle_earth_moon = 0;
 	angle_sun_earth = 0;
-	angle_incr_earth_moon = 0; // ?
-	angle_incr_sun_earth = 0; // ?
+	angle_incr_earth_moon = 0.1; // ?
+	angle_incr_sun_earth = 0.1; // ?
 
-	float sunPos [2] = {sun_x, sun_y};
-	float earthPos [2] = {earth_x, earth_y};
-	float moonPos [2] = {moon_x, moon_y};
+	double sunPos [2] = {sun_x, sun_y};
+	double earthPos [2] = {earth_x, earth_y};
+	double moonPos [2] = {moon_x, moon_y};
 	sun.setData(sunPos);
 	earth.setData(earthPos);
 	moon.setData(moonPos);
@@ -114,7 +130,7 @@ int min (int a, int b) { return a>b? a: b; }
 // timer callback function
 void timer (int value) 
 {
-	///////
+	
 	// update your variables here ...
 	//
 
@@ -130,6 +146,10 @@ void timer (int value)
 
 	//
 	///////
+	rotate(earth_x, earth_y, &moon_x, &moon_y, angle_incr_earth_moon);
+	//rotate_origin(&earth_x, &earth_y, angle_incr_sun_earth);
+	//rotate_origin(&moon_x, &moon_y, angle_incr_sun_earth);
+	
 
 	// the last two lines should always be
 	glutPostRedisplay ();
@@ -148,13 +168,22 @@ void display1 (void)
 
 	glBegin (GL_TRIANGLES);
 		glColor3f (1,0,0);
-		glVertex2i (g_iPos, 0);
+		glVertex2i ((int) sun_x, (int) sun_y);
 		glColor3f (0,1,0);
-		glVertex2i (-g_iPos, g_iPos);
+		glVertex2i ((int) earth_x, (int) earth_y);
 		glColor3f (0,0,1);
-		glVertex2i (-g_iPos, -g_iPos);
+		glVertex2i ((int) moon_x, (int) moon_y);
 	glEnd ();
-
+	//setpoint((int) earth_x, (int) earth_y);
+	//setpoint((int) moon_x, (int) moon_y);
+	/*	glVertex2i ((int) moon_x+10, (int) moon_y+10);
+		glColor3f (0,1,0);
+		glVertex2i ((int) moon_x-10, (int) moon_y+10);
+		glColor3f (0,0,1);
+		glVertex2i ((int) moon_x+10, (int) moon_y-10);
+		glColor3f (1,1,0);
+		glVertex2i ((int) moon_x-10, (int) moon_y-10);
+	*/
 	//
 	///////
 
@@ -229,6 +258,21 @@ void bhamLine (Point p1, Point p2, Color c)
 	glEnd ();
 }
 */
+void rotate_origin(double * px, double * py, double increment) {
+	double distance = sqrt(pow(*px,2)*pow(*py,2));
+	double angle = atan2(*py,*px) + increment;
+	*px = cos(angle) * distance;
+	*py = sin(angle) * distance;
+}
+
+void rotate(double px1, double py1, double* px2, double* py2, double increment) {
+	double px = *px2 - px1;
+	double py = *py2 - py1;
+	double distance = sqrt(pow(px,2)*pow(py,2));
+	double angle = atan2(py,px) + increment;
+	*px2 = cos(angle) * distance + px1;
+	*py2 = sin(angle) * distance + py1;
+}
 
 int main (int argc, char **argv) 
 {
