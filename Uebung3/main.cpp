9b8670da9d9f6054f952b28aeed89e98032a46fc
sebastianillges <sebastian.illges@gmail.com>
2022-12-16 15:18:45 +0100

    Update

--- Uebung3/main.cpp
+++ Uebung3/main.cpp
@@ -9,15 +9,19 @@
 #include "glut.h"
 #include <iostream>
 using namespace std;
-#include <map>
 
 // system relevant global variables
 // window width and height (choose an appropriate size)
-const int g_iWidth  = 2560;
-const int g_iHeight = 1600;
+const int g_iWidth  = 1080;
+const int g_iHeight = 1080;
 
 // global variable to tune the timer interval
 int g_iTimerMSecs;
+
+// cuboids
+CVec4f c1[8];
+CVec4f c2[8];
+CVec4f c3[8];
 
 class Point
 {
@@ -47,214 +51,6 @@ public:
 	}
 	float r, g, b;
 };
-map<int, Color> colormap;
-void bhamLine(Point, Point, Color);
-void aliasLine(Point, Point, Color);
-void bhamCircle(Point, int, Color);
-void rotate_origin(double *, double *, double);
-void rotate(double, double, double*, double*, double);
-CMat3d rotate_matrix(double, double, double, double, double);
-void floodFill(Point, Color);
-
-// function to initialize our own variables
-void init () 
-{
-	// init timer interval
-	g_iTimerMSecs = 10;
-
-	// init variables for display1
-
-
-	// init variables for display2
-	
-}
-
-// function to initialize the view to ortho-projection
-void initGL () 
-{
-	glViewport (0, 0, g_iWidth, g_iHeight);	// Establish viewing area to cover entire window.
-
-	glMatrixMode (GL_PROJECTION);			// Start modifying the projection matrix.
-	glLoadIdentity ();						// Reset project matrix.
-	glOrtho (-g_iWidth/2, g_iWidth/2, -g_iHeight/2, g_iHeight/2, 0, 1);	// Map abstract coords directly to window coords.
-
-	// tell GL that we draw to the back buffer and
-	// swap buffers when image is ready to avoid flickering
-	glDrawBuffer (GL_BACK);
-
-	// tell which color to use to clear image
-	glClearColor (0,0,0,1);
-}
-
-int min (int a, int b) { return a>b? a: b; }
-// timer callback function
-void timer (int value) {
-	// variables for display1 ...
-
-	// variables for display2 ...
-
-	// the last two lines should always be
-	glutPostRedisplay ();
-	glutTimerFunc (g_iTimerMSecs, timer, 0);	// call timer for next iteration
-}
-
-// display callback function
-void display1 (void) 
-{
-	glClear (GL_COLOR_BUFFER_BIT);
-	
-	// In double buffer mode the last
-	// two lines should alsways be
-	glFlush ();
-	glutSwapBuffers (); // swap front and back buffer
-}
-
-// display callback function
-void display2 (void) 
-{
-	glClear (GL_COLOR_BUFFER_BIT);
-
-	// In double buffer mode the last
-	// two lines should always be
-	glFlush ();
-	glutSwapBuffers (); // swap front and back buffer
-}
-
-void keyboard (unsigned char key, int x, int y) 
-{
-	switch (key) {
-		case 'q':
-		case 'Q':
-			exit (0); // quit program
-			break;
-		case '1':
-			glutDisplayFunc (display1);
-			//glutPostRedisplay ();	// not needed since timer triggers redisplay
-			break;
-		case '2':
-			glutDisplayFunc (display2);
-			//glutPostRedisplay ();	// not needed since timer triggers redisplay
-			break;
-		default:
-			// do nothing ...
-			break;
-	};
-}
-
-void rotate_origin(double * px, double * py, double increment) {
-	double distance = sqrt(pow(*px,2)+pow(*py,2));
-	double angle = atan2(*py,*px) + increment;
-	*px = cos(angle) * distance;
-	*py = sin(angle) * distance;
-}
-
-void rotate(double px1, double py1, double* px2, double* py2, double increment) {
-	double px = *px2 - px1;
-	double py = *py2 - py1;
-	double distance = sqrt(pow(px,2)+pow(py,2));
-	double angle = atan2(py,px) + increment;
-	*px2 = cos(angle) * distance + px1;
-	*py2 = sin(angle) * distance + py1;
-}
-
-CMat3d rotate_matrix(double px1, double py1, double px2, double py2, double angle) {
-	double rot_array[3][3];
-	rot_array[0][0] = cos(angle);
-	rot_array[0][1] = -1*sin(angle);
-	rot_array[0][2] = 0;
-	rot_array[1][0] = sin(angle);
-	rot_array[1][1] = rot_array[0][0];
-	rot_array[1][2] = 0;
-	rot_array[2][0] = 0;
-	rot_array[2][1] = 0;
-	rot_array[2][2] = 1;
-	CMat3d rotate = CMatrix<double,3>(rot_array);
-	if (px1 == 0 && py1 == 0) {
-		return rotate;
-	}
-	double t_array[3][3];
-	double t2_array[3][3];
-	t_array[0][0] = 1;
-	t_array[0][1] = 0;
-	t_array[0][2] = -px1;
-	t_array[1][0] = 0;
-	t_array[1][1] = 1;
-	t_array[1][2] = -py1;
-	t_array[2][0] = 0;
-	t_array[2][1] = 0;
-	t_array[2][2] = 1;
-	t2_array[0][0] = 1;
-	t2_array[0][1] = 0;
-	t2_array[0][2] = px1;
-	t2_array[1][0] = 0;
-	t2_array[1][1] = 1;
-	t2_array[1][2] = py1;
-	t2_array[2][0] = 0;
-	t2_array[2][1] = 0;
-	t2_array[2][2] = 1;
-	return CMat3d(t2_array)*rotate*CMat3d(t_array);
-}
-
-int abs(int a)
-{
-    return (((a) < 0) ? -(a) : a);
-}
-// Xiaolin Wu , Line Algorithm
-void aliasLine(Point p1, Point p2, Color c)
-{
-    glBegin(GL_POINTS);
-    glColor3f(c.r, c.g, c.b);
-    glVertex2i(p1.x, p1.y);
-
-    int dx = p2.x - p1.x;
-    int ax = abs(dx) << 1;
-    int sx = (((dx) < 0) ? -1 : 1);
-
-    int dy = p2.y - p1.y;
-    int ay = abs(dy) << 1;
-    int sy = (((dy) < 0) ? -1 : 1);
-
-    if (ax > ay)
-    {
-
-        int d = ay - (ax >> 1);
-        while (p1.x != p2.x)
-        {
-            glColor3f(c.r, c.g, c.b);
-            glVertex2i(p1.x, p1.y);
-
-            if (d >= 0)
-            {
-                p1.y += sy;
-                d -= ax;
-            }
-            p1.x += sx;
-            d += ay;
-        }
-    }
-    else
-    {
-
-        int d = ax - (ay >> 1);
-        while (p1.y != p2.y)
-        {
-            glColor3f(c.r, c.g, c.b);
-            glVertex2i(p1.x, p1.y);
-            if (d >= 0)
-            {
-                p1.x += sx;
-                d -= ay;
-            }
-            p1.y += sy;
-            d += ax;
-        }
-    }
-
-    glColor3f(c.r, c.g, c.b);
-    glVertex2i(p1.x, p1.y);
-
-    glEnd();
-}
 
 void bhamLine(Point p1, Point p2, Color c)
 {
@@ -307,8 +103,8 @@ void bhamLine(Point p1, Point p2, Color c)
 		}
 	glEnd();
 }
-
-void bhamCircle(Point p, int r, Color c) {
+void bhamCircle(Point p, int r, Color c)
+{
   // Mittelpunkt
 	glBegin(GL_POINTS);
 		glColor3f(c.r,c.g,c.b);
@@ -364,8 +160,188 @@ void bhamCircle(Point p, int r, Color c) {
 	//glEnd();
 }
 
+CVec4f projectZ(float fFocus, CVec4f pView)
+{
+	CVec3f viewOrigin;
+	CVec3f viewDir;
+	CVec3f viewUp;
+	CVec3f viewLeft;
+	float o_arr[3] = {0, 0, 0};
+	float d_arr[3] = {0, 0, -1};
+	float u_arr[3] = {0, 1, 0};
+	float l_arr[3] = {1, 0, 0};
+	viewOrigin.setData(o_arr);
+	viewDir.setData(d_arr);
+	viewUp.setData(u_arr);
+	viewLeft.setData(l_arr);
+
+	CVec3f eyePoint;
+	float e_arr[3] = {0, 0, fFocus};
+	eyePoint.setData(e_arr);
+
+	
+	float m_mat[4][4] = {{1, 0, 0, 0}, {0, 1, 0, 0}, {0, 0, 0, 0}, {0, 0, 1/-fFocus, 1}};
+	CMat4f m = CMatrix<float,4>(m_mat);
+
+	//return pView * m;
+	float x = pView.get(0);
+	float y = pView.get(1);
+	float z = pView.get(2);
+	float k = fFocus / (fFocus - z);
+
+	float xn = x * k;
+	float yn = y * k;
+
+	CVec4f pNew;
+	float pNewArr[4] = {xn, yn, 0, 1};
+	pNew.setData(pNewArr);
+	return pNew;
+
+}
+
+void drawProjektedZ(CVec4f points[8], Color c) {
+	Point Points[8];
+	for (int i = 0; i < 8; i++) {
+		Points[i] = Point((int) points[i].get(0), (int) points[i].get(1));
+	}
+	bhamLine(Points[0], Points[1], c);
+	bhamLine(Points[0], Points[3], c);
+	bhamLine(Points[0], Points[4], c);
+	bhamLine(Points[1], Points[2], c);
+	bhamLine(Points[1], Points[5], c);
+	bhamLine(Points[2], Points[3], c);
+	bhamLine(Points[2], Points[6], c);
+	bhamLine(Points[3], Points[7], c);
+	bhamLine(Points[4], Points[5], c);
+	bhamLine(Points[4], Points[7], c);
+	bhamLine(Points[5], Points[6], c);
+	bhamLine(Points[6], Points[7], c);
+}
+
+//     5------6
+//    /|     /|
+//   1------2 |
+//   | 4----|-7
+//   |/     |/
+//   0------3
+
+void drawQuader(CVec4f Cuboid[8],float fFocus, Color c) {
+	CVec4f points[8];
+	for (int i = 0; i < 8; i++) {
+		points[i] = projectZ(fFocus, Cuboid[i]);
+	}
+	drawProjektedZ(points, c);
+}
+
+// function to initialize our own variables
+void init () 
+{
+	// init timer interval
+	g_iTimerMSecs = 10;
+
+	// init variables for display1
+	float c1p0[4] = {-100, -100, 100, 1};
+	float c1p1[4] = {-100, 300, 100, 1};
+	float c1p2[4] = {300, 300, 100, 1};
+	float c1p3[4] = {300, -100, 100, 1};
+	float c1p4[4] = {-100, -100, 500, 1};
+	float c1p5[4] = {-100, 300, 500, 1};
+	float c1p6[4] = {300, 300, 500, 1};
+	float c1p7[4] = {300, -100, 500, 1};
+	
+	c1[0].setData(c1p0);
+	c1[1].setData(c1p1);
+	c1[2].setData(c1p2);
+	c1[3].setData(c1p3);
+	c1[4].setData(c1p4);
+	c1[5].setData(c1p5);
+	c1[6].setData(c1p6);
+	c1[7].setData(c1p7);
+
+	// init variables for display2
+	
+}
+
+// function to initialize the view to ortho-projection
+void initGL () 
+{
+	glViewport (0, 0, g_iWidth, g_iHeight);	// Establish viewing area to cover entire window.
+
+	glMatrixMode (GL_PROJECTION);			// Start modifying the projection matrix.
+	glLoadIdentity ();						// Reset project matrix.
+	glOrtho (-g_iWidth/2, g_iWidth/2, -g_iHeight/2, g_iHeight/2, 0, 1);	// Map abstract coords directly to window coords.
+
+	// tell GL that we draw to the back buffer and
+	// swap buffers when image is ready to avoid flickering
+	glDrawBuffer (GL_BACK);
+
+	// tell which color to use to clear image
+	glClearColor (0,0,0,1);
+}
+
+int min (int a, int b) { return a>b? a: b; }
+// timer callback function
+void timer (int value) {
+	// variables for display1 ...
+
+	// variables for display2 ...
+
+	// the last two lines should always be
+	glutPostRedisplay ();
+	glutTimerFunc (g_iTimerMSecs, timer, 0);	// call timer for next iteration
+}
+
+// display callback function
+void display1 (void) 
+{
+	glClear (GL_COLOR_BUFFER_BIT);
+	drawQuader(c1, -1000, Color(1,0,0));
+	// In double buffer mode the last
+	// two lines should alsways be
+	glFlush ();
+	glutSwapBuffers (); // swap front and back buffer
+}
+
+// display callback function
+void display2 (void) 
+{
+	glClear (GL_COLOR_BUFFER_BIT);
+
+	// In double buffer mode the last
+	// two lines should always be
+	glFlush ();
+	glutSwapBuffers (); // swap front and back buffer
+}
+
+void keyboard (unsigned char key, int x, int y) 
+{
+	switch (key) {
+		case 'q':
+		case 'Q':
+			exit (0); // quit program
+			break;
+		case '1':
+			glutDisplayFunc (display1);
+			//glutPostRedisplay ();	// not needed since timer triggers redisplay
+			break;
+		case '2':
+			glutDisplayFunc (display2);
+			//glutPostRedisplay ();	// not needed since timer triggers redisplay
+			break;
+		default:
+			// do nothing ...
+			break;
+	};
+}
+
+void resize(int width, int height) {
+    // we ignore the params and do:
+    glutReshapeWindow(1080, 1080);
+}
+
 int main (int argc, char **argv) 
 {
+
 	glutInit (&argc, argv);
 	// we have to use double buffer to avoid flickering
 	// TODO: lookup "double buffer", what is it for, how is it used ...
@@ -379,6 +355,7 @@ int main (int argc, char **argv)
 	glutTimerFunc (10, timer, 0);
 	glutKeyboardFunc (keyboard);
 	glutDisplayFunc (display1);
+	//glutReshapeFunc(resize);
 	// you might want to add a resize function analog to
 	// �bung1 using code similar to the initGL function ...
 
